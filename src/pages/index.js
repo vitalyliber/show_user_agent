5bf4a4533b519ddfb862ea54339fed0f5aad1bc2
vitaly liber <zenamax@gmail.com>
2023-02-16 20:58:06 +0300

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import {useEffect, useState} from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [data, setData] = useState("")
+  useEffect(() => {
+    setData(window.navigator.userAgent)
+  }, [])
   return (
     <>
       <Head>
@@ -16,10 +21,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
+          <p>userAgent: {data}</p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
